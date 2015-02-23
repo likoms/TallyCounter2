5686786505cb19ad3d4ccee4bc056f1ad7c374ae
wtrocki <wtrocki@gmail.com>
2015-02-23 20:44:34 +0000

    Poprawki tak by kodzik dzialal na telefonie :)

--- app/src/main/java/com/likoms/tallycounter2/TallyMain.java
+++ app/src/main/java/com/likoms/tallycounter2/TallyMain.java
@@ -1,48 +1,91 @@
 package com.likoms.tallycounter2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.InputType;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 
 import com.likoms.tallycounter2.fragment.PlaceholderFragment;
 
 
 public class TallyMain extends ActionBarActivity {
 
+    private PlaceholderFragment placeholderFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tally_main);
         if (savedInstanceState == null) {
+            placeholderFragment = new PlaceholderFragment();
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, placeholderFragment)
                     .commit();
         }
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tally_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-       /* if (id == R.id.action_settings) {
-            return true;
-        }*/
-
-        return super.onOptionsItemSelected(item);
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.new_game:
+                placeholderFragment.reset();
+                return true;
+            case R.id.settings:
+                showSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU){
+            openOptionsMenu();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void showSettings() {
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        final NumberPicker numberPicker = new NumberPicker(this);
+        final EditText inputLimit = new EditText(this);
+        inputLimit.setInputType(InputType.TYPE_CLASS_NUMBER);
+        numberPicker.setMaxValue(50);
+        numberPicker.setMinValue(10);
+        numberPicker.setWrapSelectorWheel(false);
+        alert.setMessage("Edit limit");
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        placeholderFragment.reset();
+                        placeholderFragment.setLimit(numberPicker.getValue());
+                    }
+                }
+        );
+        alert.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.cancel();
+                    }
+                }
+        );
+        alert.setView(numberPicker);
+        alert.create().show();
+    }
 
 }
--- app/src/main/java/com/likoms/tallycounter2/fragment/PlaceholderFragment.java
+++ app/src/main/java/com/likoms/tallycounter2/fragment/PlaceholderFragment.java
@@ -4,16 +4,10 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.InputType;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import com.likoms.tallycounter2.R;
@@ -28,41 +22,21 @@ public class PlaceholderFragment extends Fragment {
     private Button nextButton;
     private Button nextButton2;
 
+    private int limit;
+
 
     public PlaceholderFragment() {
+        setupLimit();
     }
-
 
     // TODO change choosing value in preference
     // TODO fix Dialog Alert
     // TODO fix bug : When one is pressed second can't be pressed
 
-    private Integer DEFAULT_LIMIT = 10;
+    private final Integer DEFAULT_LIMIT = 10;
     private final static int DEFAULT_INCREMENT = 1;
     private final static int DEFAULT_VALUE = 0;
 
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        // Do something that differs the Activity's menu here
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.menu_placeholder_fragment, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.new_game:
-                reset();
-                return true;
-            case R.id.settings:
-                showSettings();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -70,40 +44,28 @@ public class PlaceholderFragment extends Fragment {
         setHasOptionsMenu(true);
         View rootView = inflater.inflate(R.layout.fragment_tally_main, container, false);
         this.counter = (TextView) rootView.findViewById(R.id.counter);
-        counter.setRotation(180);
+        this.counter.setRotation(180);
         this.counter2 = (TextView) rootView.findViewById(R.id.counter2);
         this.nextButton = (Button) rootView.findViewById(R.id.next);
-        nextButton.setRotation(180);
+        this.nextButton.setRotation(180);
         this.nextButton2 = (Button) rootView.findViewById(R.id.next2);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (incrementElement(counter)) {
-                    if (nextButton2.isPressed() && incrementElement(counter2)) {
-                        onGameFinished(getActivity().getString(R.string.draw));
-                    } else {
-                        onGameFinished(getActivity().getString(R.string.red_win));
-                    }
+                    onGameFinished(getActivity().getString(R.string.red_win));
                 }
-
             }
         });
 
         nextButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 if (incrementElement(counter2)) {
-
-                    if (nextButton.isPressed() && incrementElement(counter)) {
-                        onGameFinished(getActivity().getString(R.string.draw));
-                    } else {
-                        onGameFinished(getActivity().getString(R.string.blue_win));
-                    }
+                    onGameFinished(getActivity().getString(R.string.blue_win));
                 }
             }
         });
-
         return rootView;
     }
 
@@ -126,7 +88,7 @@ public class PlaceholderFragment extends Fragment {
      */
     private boolean incrementElement(TextView counter) {
         int index = Integer.parseInt(counter.getText().toString());
-        if (index == DEFAULT_LIMIT) {
+        if (index == limit) {
             return true;
         }
         counter.setText((index + DEFAULT_INCREMENT) + "");
@@ -138,60 +100,13 @@ public class PlaceholderFragment extends Fragment {
         counter2.setText(DEFAULT_VALUE + "");
     }
 
-
-    private void showSettings() {
-        final AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
-        final NumberPicker numberPicker = new NumberPicker(getActivity());
-        final EditText inputLimit = new EditText(getActivity());
-        inputLimit.setInputType(InputType.TYPE_CLASS_NUMBER);
-        // alert View
-        String[] values = new String[100];
-        for (int i = 1; i < values.length; i++) {
-            values[i] = Integer.toString(i * 5);
-        }
-        numberPicker.setMaxValue(values.length - 1);
-        numberPicker.setMinValue(0);
-        numberPicker.setWrapSelectorWheel(false);
-        numberPicker.setDisplayedValues(values);
-
-        //
-
-
-        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker numberPicker, int oldVal, int newVal) {
-                    DEFAULT_LIMIT = newVal;
-
-            }
-        });
-
-        alert.setMessage("Edit limit");
-        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener()
-
-                {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        // String valueLimit = inputLimit.getText().toString().trim();
-                        //DEFAULT_LIMIT = Integer.parseInt(valueLimit);
-                        // Integer valueLimit = numberPicker.getValue();
-                        // DEFAULT_LIMIT = valueLimit;
-
-                    }
-                }
-
-        );
-
-        alert.setNegativeButton("Cancel",
-                new DialogInterface.OnClickListener()
-                {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        dialog.cancel();
-                    }
-                }
-        );
-        alert.setView(numberPicker);
-        alert.create().show();
+    private void setupLimit() {
+        this.limit = DEFAULT_LIMIT;
     }
 
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
 }
 
 
